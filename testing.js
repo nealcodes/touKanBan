2aa041cc1294bff964e84f5aad93e60e6fd69bb1
Neal Duncan <nealcodes@gmail.com>
2018-02-19 15:31:22 -0500

    added card ID assignment func

--- testing.js
+++ testing.js
@@ -1,12 +1,26 @@
+const counterId = (function () {
+    var counter = 0;
 
+    var assign = function () {
+        this.counter++;
+        return this.counter;
+    }
+
+    return {
+        counter,
+        assign
+    };
+})();
 
 const Card = function () {
     var t = document.querySelector("#Card");
+    var card = t.content.querySelector('.card');
+    card.dataset.cardId = counterId.assign();
     var list = document.querySelector('.list');
     var clone = document.importNode(t.content, true);
     list.insertBefore(clone, list.lastElementChild);
 
-    
+
 };
 
 const List = [];
